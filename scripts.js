fb37157e6a36bff871bf710d3720cea8b9b738bf
kiquino <kiquino@gmail.com>
2022-03-23 19:39:21 -0300

    agrego script

--- scripts.js
+++ scripts.js
@@ -43,3 +43,111 @@ $(document).on('scroll', function () {
 })
 
 //FIN EVENTO SCROLL
+//Envio de Mail - FORM DESKTOP
+
+$("#form-desktop").submit(function (event) {
+    event.preventDefault();
+
+    if ($("#mensaje").val() != '') {
+        $.post("email.php", {
+            nombre: $("#nombre").val(),
+
+            mail: $("#email").val(),
+            tel: $("#tel").val(),
+            text: $("#mensaje").val()
+        }, function (data, status) {
+            if (data) {
+                $("#mensaje-result").append("<div class='success' id='ap-men'>Mensaje enviado</div>");
+
+                setTimeout(() => {
+
+                    $("#ap-men").remove();
+                }, 15000);
+                $("#mensaje").val('');
+                $("#tel").val('');
+                $("#email").val('');
+                $("#nombre").val('');
+
+
+            } else {
+                $("#mensaje-result").append("<div class='fail' id='ap-men'>Mensaje no enviado</div>");
+
+                setTimeout(() => {
+
+                    $("#ap-men").remove();
+                }, 15000);
+
+            }
+        });
+    } else {
+        $("#mensaje-result").append("<div class='fail' id='ap-men'>Debe escribir un mensaje</div>");
+
+        setTimeout(() => {
+
+            $("#ap-men").remove();
+        }, 15000);
+
+    }
+
+
+
+
+
+});
+//FIN FORM DESKTOP
+
+
+//Envio de Mail - FORM MOBILE
+
+$("#form-mobile").submit(function (event) {
+    event.preventDefault();
+
+    if ($("#mensaje-mobile").val() != '') {
+        $.post("email.php", {
+            nombre: $("#nombre-mobile").val(),
+
+            mail: $("#email-mobile").val(),
+            tel: $("#tel-mobile").val(),
+            text: $("#mensaje-mobile").val(),
+
+            agree: $("#agree").val()
+        }, function (data, status) {
+            if (data) {
+                $("#mensaje-result").append("<div class='success' id='ap-men'>Mensaje enviado</div>");
+
+                setTimeout(() => {
+
+                    $("#ap-men").remove();
+                }, 15000);
+                $("#mensaje-mobile").val('');
+                $("#tel-mobile").val('');
+                $("#email-mobile").val('');
+                $("#nombre-mobile").val('');
+
+
+            } else {
+                $("#mensaje-result").append("<div class='fail' id='ap-men'>Mensaje no enviado</div>");
+
+                setTimeout(() => {
+
+                    $("#ap-men").remove();
+                }, 15000);
+
+            }
+        });
+    } else {
+        $("#mensaje-result").append("<div class='fail' id='ap-men'>Debe escribir un mensaje</div>");
+
+        setTimeout(() => {
+
+            $("#ap-men").remove();
+        }, 15000);
+
+    }
+
+
+
+
+
+});
+//FIN FORM MOBILE

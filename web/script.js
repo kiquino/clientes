60f87992d3fc3f7cb4e83cfac898c9ec98b4e5df
kiquino <kiquino@gmail.com>
2022-04-17 19:26:42 -0300

    .

--- web/script.js
+++ web/script.js
@@ -76,77 +76,77 @@ $("#form-desktop").submit(function (event) {
 
 //Envio de Mail - FORM MOBILE
 
-$("#form-mobile").submit(function (event) {
-    event.preventDefault();
-    $("#submit-mobile").prop('disabled', true);
-    $("#nombre-mobile").prop('disabled', true);
-    $("#email-mobile").prop('disabled', true);
-    $("#mensaje-mobile").prop('disabled', true);
-    $("#tel-mobile").prop('disabled', true);
+// $("#form-mobile").submit(function (event) {
+//     event.preventDefault();
+//     $("#submit-mobile").prop('disabled', true);
+//     $("#nombre-mobile").prop('disabled', true);
+//     $("#email-mobile").prop('disabled', true);
+//     $("#mensaje-mobile").prop('disabled', true);
+//     $("#tel-mobile").prop('disabled', true);
 
-    if ($("#mensaje-mobile").val() != '') {
-        $.post("mail.php", {
-            nombre: $("#nombre-mobile").val(),
+//     if ($("#mensaje-mobile").val() != '') {
+//         $.post("mail.php", {
+//             nombre: $("#nombre-mobile").val(),
 
-            mail: $("#email-mobile").val(),
-            tel: $("#tel-mobile").val(),
-            text: $("#mensaje-mobile").val(),
+//             mail: $("#email-mobile").val(),
+//             tel: $("#tel-mobile").val(),
+//             text: $("#mensaje-mobile").val(),
 
-            agree: $("#agree").val()
-        }, function (data, status) {
-            console.log(status)
-            if (data) {
-                Swal.fire({
-                    title: '¡Gracias!',
-                    text: 'Su mensaje ha sido enviado exitosamente',
-                    icon: 'success',
-                    confirmButtonText: 'Salir'
-                })
-                $("#submit-mobile").prop('disabled', false);
-                $("#mensaje-mobile").prop('disabled', false);
-                $("#nombre-mobile").prop('disabled', false);
-                $("#email-mobile").prop('disabled', false);
-                $("#tel-mobile").prop('disabled', false);
-                setTimeout(() => {
+//             agree: $("#agree").val()
+//         }, function (data, status) {
+//             console.log(status)
+//             if (data) {
+//                 Swal.fire({
+//                     title: '¡Gracias!',
+//                     text: 'Su mensaje ha sido enviado exitosamente',
+//                     icon: 'success',
+//                     confirmButtonText: 'Salir'
+//                 })
+//                 $("#submit-mobile").prop('disabled', false);
+//                 $("#mensaje-mobile").prop('disabled', false);
+//                 $("#nombre-mobile").prop('disabled', false);
+//                 $("#email-mobile").prop('disabled', false);
+//                 $("#tel-mobile").prop('disabled', false);
+//                 setTimeout(() => {
 
-                    $("#ap-men").remove();
-                }, 15000);
-                $("#mensaje-mobile").val('');
-                $("#tel-mobile").val('');
-                $("#email-mobile").val('');
-                $("#nombre-mobile").val('');
-
-
-            } else {
-                Swal.fire({
-                    title: 'Error',
-                    text: 'El mensaje no pudo ser enviado. Disculpamos las molestias.',
-                    icon: 'error',
-
-                    confirmButtonText: 'Adelante!',
-
-                })
+//                     $("#ap-men").remove();
+//                 }, 15000);
+//                 $("#mensaje-mobile").val('');
+//                 $("#tel-mobile").val('');
+//                 $("#email-mobile").val('');
+//                 $("#nombre-mobile").val('');
 
 
-                setTimeout(() => {
+//             } else {
+//                 Swal.fire({
+//                     title: 'Error',
+//                     text: 'El mensaje no pudo ser enviado. Disculpamos las molestias.',
+//                     icon: 'error',
 
-                    $("#ap-men").remove();
-                }, 15000);
+//                     confirmButtonText: 'Adelante!',
 
-            }
-        });
-    } else {
-        $("#mensaje-result2").append("<div class='fail' id='ap-men'>Debe escribir un mensaje</div>");
-
-        setTimeout(() => {
-
-            $("#ap-men").remove();
-        }, 5000);
-
-    }
+//                 })
 
 
+//                 setTimeout(() => {
+
+//                     $("#ap-men").remove();
+//                 }, 15000);
+
+//             }
+//         });
+//     } else {
+//         $("#mensaje-result2").append("<div class='fail' id='ap-men'>Debe escribir un mensaje</div>");
+
+//         setTimeout(() => {
+
+//             $("#ap-men").remove();
+//         }, 5000);
+
+//     }
 
 
 
-});
+
+
+// });
